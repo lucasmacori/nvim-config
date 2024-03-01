05f6935508e99914149175cbe22b22ffbcaf8bbf
Lucas MACORI <25822355+lucasmacori@users.noreply.github.com>
2024-03-02 00:00:50 +0100

    Added Git, Harpoon, Treesitter and undotree plugins

--- lua/lucas/packer.lua
+++ lua/lucas/packer.lua
@@ -18,4 +18,7 @@ return require('packer').startup(function(use)
   })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('ThePrimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
 end)
